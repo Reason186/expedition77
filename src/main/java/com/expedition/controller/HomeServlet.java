7fea31610b6c368266ebf5d6a492a69bcfd6d931
kaushubh <kaushubh3@gmail.com>
2026-04-15 17:48:58 +0545

    Creating homepage.jsp and home.css

--- src/main/java/com/expedition/controller/HomeServlet.java
+++ src/main/java/com/expedition/controller/HomeServlet.java
@@ -27,7 +27,7 @@ public class HomeServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.getRequestDispatcher("/WEB-INF/pages/home.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/pages/HomePage.jsp").forward(request, response);
 	}
 
 	/**
